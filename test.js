bcdc53dca9b4863b61b0a9a195c05b135248ba16
julio1980p <perezduranjulio@gmail.com>
2020-03-20 20:05:22 -0400

    login y pruebas

--- test.js
+++ test.js
@@ -3,7 +3,18 @@ describe("Hacer login", function(){
 
 	it("Hacer login", function(){
 		browser.get("http://localhost:4200/login");
-		browser.sleep(20000)
+		var username = browser.findElement(protractor.by.css('[name="username"]'));
+		var password = browser.findElement(protractor.by.css('[name="password"]'));
+		var submit = browser.findElement(protractor.by.css('[type="submit"]'));
+		username.sendKeys('yo');
+		password.sendKeys('1');
+
+		submit.click();
+		
+	});
+
+	it("hacer pruebas", function(){
+		browser.get("http://localhost:4200/projects");
 		
 	});
 });
