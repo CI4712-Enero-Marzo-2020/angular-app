dbbfecd213a4bb8eaa38d46da0e8c928110d455f
julio1980p <perezduranjulio@gmail.com>
2020-03-20 20:28:08 -0400

    pruebas unitarias

--- test.js
+++ test.js
@@ -10,11 +10,37 @@ describe("Hacer login", function(){
 		password.sendKeys('1');
 
 		submit.click();
+		browser.sleep(2500);
 		
 	});
 
 	it("hacer pruebas", function(){
 		browser.get("http://localhost:4200/projects");
+		var agregar = browser.findElement(protractor.by.css('[id="but2"]'));
+		agregar.click();
+		browser.sleep(2500);
+		var nombreProy = browser.findElement(protractor.by.css('[id="recipient-name"]'));
+		nombreProy.sendKeys('Proyecto Prueba');
+		browser.sleep(2500);
+		var submit = browser.findElement(protractor.by.css('[class="btn btn-primary"]'));
+		submit.click();
+		browser.sleep(2500);
+		agregar.click();
+		browser.sleep(2500);
+		nombreProy.clear();
+		nombreProy.sendKeys('Proyecto Prueba 2');
+		browser.sleep(2500);
+		submit.click();
+		browser.sleep(2500);
+		agregar.click();
+		browser.sleep(2500);
+		nombreProy.clear();
+		nombreProy.sendKeys('Proyecto Prueba 3');
+		browser.sleep(2500);
+		submit.click();
+
+		//agregar proyecto
+
 		
 	});
 });
