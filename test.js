29415807a95e161858f59e4dca4e57f5215eadf4
julio1980p <perezduranjulio@gmail.com>
2020-03-29 18:56:39 -0400

    modificaciones pruebas

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ describe("Hacer login", function(){
 		var password = browser.findElement(protractor.by.css('[name="password"]'));
 		var submit = browser.findElement(protractor.by.css('[type="submit"]'));
 		username.sendKeys('yo');
-		password.sendKeys('1');
+		password.sendKeys('1234');
 
 		submit.click();
 		browser.sleep(400);
@@ -51,6 +51,10 @@ describe("Hacer login", function(){
 		browser.sleep(400);
 		nombreHist.sendKeys('Historia Prueba 1');
 		browser.sleep(400);
+		var prioridadHist = browser.findElement(protractor.by.css('[id="prioridad"]'));
+		browser.sleep(400);
+		prioridadHist.click();
+		browser.sleep(400);
 		var submit = browser.findElement(protractor.by.css('[class="btn btn-primary"]'));
 		submit.click();
 		browser.sleep(400);
@@ -58,11 +62,15 @@ describe("Hacer login", function(){
 		browser.sleep(400);
 		nombreHist.sendKeys('Historia Prueba 2');
 		browser.sleep(400);
+		prioridadHist.click();
+		browser.sleep(400);
 		submit.click()
 		browser.sleep(400);
 		agregar2.click();
 		browser.sleep(400);
 		nombreHist.sendKeys('Epica Prueba 1');
+		browser.sleep(400);
+		prioridadHist.click();
 		browser.sleep(400);
 		var epica = browser.findElement(protractor.by.css('[class="check"]'));
 		browser.sleep(400);
