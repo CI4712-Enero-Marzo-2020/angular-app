cc6345da642812ef3560f7b546129460d9b08cc6
Constanza Abarca <costi.abarca@gmail.com>
2020-04-06 00:17:58 -0400

    Añadido documento de introduccion

--- src/app/intro/intro.component.spec.ts
+++ src/app/intro/intro.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { IntroComponent } from './intro.component';
+import { ProjectsComponent } from './projects.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 describe('IntroComponent', () => {
   let component: IntroComponent;
@@ -9,6 +14,7 @@ describe('IntroComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ IntroComponent ]
+      imports: [FontAwesomeModule, FormsModule, ReactiveFormsModule, RouterTestingModule, HttpClientTestingModule],
     })
     .compileComponents();
   }));
--- src/app/services/intro/intro.service.ts
+++ src/app/services/intro/intro.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { AuthService } from '../users/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class IntroService {
 
-  constructor() { }
+export class IntroService {
+	
+	url = environment.api;
+  userID: any;
+  doc_id: any;
+  constructor(private http: HttpClient, private authService: AuthService) { 
+  	if (authService.getCurrentUser()) {
+      this.userID = authService.getCurrentUser().userId;
+    }
+  }
+
+  create_intro(content, project) {
+
+    this.doc_id = this.http.get(`${this.url}docs/get/${project}`)
+  	this.http.post(`${this.url}docs/add/intro/`, {doc_id:this.doc_id, content:content})
+
+    .toPromise()
+    .then(
+      (response: any[]) => {
+        return response;
+    },
+      (error) => {
+        console.log(error);
+        return null;
+      }
+    );
+  }
+
 }
