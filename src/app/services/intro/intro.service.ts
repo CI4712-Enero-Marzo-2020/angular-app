85d7e81565ac27517c0214fdf11e060bdadbd246
Constanza Abarca <costi.abarca@gmail.com>
2020-04-06 02:46:54 -0400

    Cambiando formato del post request a form-data

--- src/app/services/intro/intro.service.ts
+++ src/app/services/intro/intro.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders} from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { AuthService } from '../users/auth.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,7 @@ export class IntroService {
 	url = environment.api;
   userID: any;
   doc_id: any;
+
   constructor(private http: HttpClient, private authService: AuthService) { 
   	if (authService.getCurrentUser()) {
       this.userID = authService.getCurrentUser().userId;
@@ -21,7 +23,11 @@ export class IntroService {
   create_intro(content, project) {
 
     this.doc_id = this.http.get(`${this.url}docs/get/${project}`)
-  	this.http.post(`${this.url}docs/add/intro/`, {doc_id:this.doc_id, content:content})
+    const formData = new FormData();
+    formData.append('content', content);
+    formData.append('doc_id', this.doc_id );
+
+  	this.http.post(`${this.url}docs/add/intro/`, formData)
 
     .toPromise()
     .then(
