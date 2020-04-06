3016f81985751f455d8ced9c9db26203b12cc506
Constanza Abarca <costi.abarca@gmail.com>
2020-04-06 00:17:58 -0400

    Añadido documento de introduccion

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,23 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/users/auth.service';
+import { IntroService } from '../services/intro/intro.service';
 
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
   styleUrls: ['./intro.component.scss']
 })
-export class IntroComponent implements OnInit {
 
-  constructor() { }
+export class IntroComponent implements OnInit {
+  
+  project: string;
+  content: string;
+  addIntroForm: FormGroup;
+  userID: any;
+
+  constructor(
+    private IntroService: IntroService,
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authService: AuthService
+    ) {
+    // this.getAllDocs();
+    if (authService.getCurrentUser()) {
+      this.userID = authService.getCurrentUser().userId;
+    }
+  }
 
   ngOnInit() {
+    this.addIntroForm = this.formBuilder.group({
+      project: ['', Validators.required],
+      content: ['', Validators.required],
+    });
   }
 
-  intro(){
-    console.log(this.intro);
+  async addIntro() {
+    const formData = new FormData();
+    formData.append('content', this.addIntroForm.get('content').value);
+    formData.append('doc_id', this.addIntroForm.get('doc_id').value);
+    const newIntro = await this.IntroService.create_intro(this.addIntroForm.get('content').value, this.addIntroForm.get('doc_id').value);
   }
 
-  limpiarCampos(){
-    //this.intro = '';
+  limpiarCampos() {
+    this.project = '';
+    this.content = '';
   }
 
 }
--- src/app/services/intro/intro.service.spec.ts
+++ src/app/services/intro/intro.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { IntroService } from './intro.service';
 
 describe('IntroService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+  	imports: [HttpClientTestingModule],
+    providers: [IntroService]
+  	}));
 
   it('should be created', () => {
     const service: IntroService = TestBed.get(IntroService);
